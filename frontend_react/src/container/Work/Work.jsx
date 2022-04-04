41ce90db2ee3b4c7ab4d33aed3bf50b6fb06d458
Magdalena Sarandeva <91531129+magdalenasar@users.noreply.github.com>
2022-04-04 23:33:09 +0200

    Resolving bug in Work.jsx: icons visibility

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -6,7 +6,7 @@ import { urlFor, client } from "../../client";
 import './Work.scss';
 
 const Work = () => {
-  const [activeFilter, setActiveFilter] = useState('All');
+  const [activeFilter, setActiveFilter] = useState( 'All' );
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
@@ -55,23 +55,24 @@ const Work = () => {
 
       <motion.div
         animate={animateCard}
-        transition={{duration: 0.5, delayChildren: 0.5}}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__work-portfolio"
       >
-      {filterWork.map((work, index) => (
+      { filterWork.map((work, index) => (
         <div key={index} className="app__work-item app__flex"> 
-          <div className='app__work-img app__flex'>
+          <div className='app__work-img app__flex' >
             <img src={urlFor(work.imgUrl)} alt={work.name} />
+            
             <motion.div
               className="app__work-hover app__flex"
-              whileHover={{ scale: [0, 1] }}
+              whileHover={{ opacity: [0, 1] }}
               transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
             >
               <a href={work.projectLink} target="_blank" rel="noreferrer">
                 <motion.div 
                   className="app__flex"
                   whileInView={{ scale: [0, 1] }}
-                  whileHover={{ opacity: [1, 0.9] }}
+                  whileHover={{ scale: [1, 0.9] }}
                   transition={{ duration: 0.25 }}
                 >
                 <AiFillEye />
@@ -82,7 +83,7 @@ const Work = () => {
                 <motion.div 
                   className="app__flex"
                   whileInView={{ scale: [0, 1] }}
-                  whileHover={{ opacity: [1, 0.9] }}
+                  whileHover={{ scale: [1, 0.9] }}
                   transition={{ duration: 0.25 }}
                 >
                 <AiFillGithub />
@@ -95,10 +96,10 @@ const Work = () => {
 
           <div className="app__work-content app__flex">
             <h4 className="bold-text">{work.title}</h4>
-            <p className="p-text" style={{marginTop: 10}}>{work.description}</p>
+            <p className="p-text" style={{marginTop: 10}}>{ work.description }</p>
 
             <div className="app__work-tag app__flex">
-              <p className="p-text">{work.tags[0]}</p>
+              <p className="p-text">{ work.tags[0] }</p>
             </div>
           </div>
         </div>
