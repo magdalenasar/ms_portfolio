c044a538246adccdcb89dde47144aad25b20241a
Magdalena Sarandeva <91531129+magdalenasar@users.noreply.github.com>
2022-04-04 23:41:49 +0200

    Starting implementing the Skills

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+//dependencies:
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import ReactTooltip from 'react-tooltip';
+
+
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
 import './Skills.scss';
 
